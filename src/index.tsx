04d8f72a09f5e732c8f11d3db9e1df937fb11379
Jacob GI <jacob.demmane@gmail.com>
2021-10-11 03:10:37 +0300

    all good, login design done, now foucs on firebase authen

--- src/index.tsx
+++ src/index.tsx
@@ -2,12 +2,30 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+import { doc, getDoc } from "firebase/firestore";
+
+// The Cloud Functions for Firebase SDK to create Cloud Functions and set up triggers.
 const functions = require("firebase-functions");
+
+// The Firebase Admin SDK to access Firestore.
 const admin = require("firebase-admin");
 admin.initializeApp();
+
+exports.getScreams = functions.https.onRequest((req: any, res: any) => {
+  admin
+    .firestore()
+    .collection("screams")
+    .get()
+    .then((data: any) => {
+      let screams: any = [];
+      data.forEach((doc: any) => screams.push(doc.data()));
+      return res.json(screams);
+    })
+    .catch((err: any) => console.error(err));
+});
+
 ReactDOM.render(
   <React.StrictMode>
-    functions.https.on
     <App />
   </React.StrictMode>,
   document.getElementById("root")
