b38500b8bb60699136f467872b9f1a45fc590959
Jacob GI <jacob.demmane@gmail.com>
2021-10-11 20:51:47 +0300

    all good, made a better architecture for the website

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,12 @@
 import React from "react";
 import "./css/App.css";
-import Login from "./Login/Login";
-import Social from "./Social/Social";
-import Footer from "./Login/Footer";
+// import Login from "./Login/Login";
+import Social from "./Main/Screams/Social";
 
 const App: React.FC = () => {
   return (
     <div className='App'>
-      {/* VIDEO BACKGROUND */}
-      {/* <video autoPlay muted loop id='video-bg' src='./imgs/bg.mp4' /> */}
-      <Login />
+      <Social />
     </div>
   );
 };
---
+++ src/Main/Screams/Social.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Header from "../../Header/Header";
+const Social: React.FC = () => {
+  return (
+    <div className='SocialMedia'>
+      <Header />
+      <div className='Social'>
+        <h1>Hello in Social App</h1>
+      </div>
+    </div>
+  );
+};
+export default Social;
